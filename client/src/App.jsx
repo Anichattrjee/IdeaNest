ae53af150838389de17145721a5131601907d438
Aniruddha Chatterjee <anichat1999@gmail.com>
2024-07-08 19:11:12 +0530

    Profile Page UI Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
@@ -7,24 +7,25 @@ import Signin from "./pages/Signin.jsx";
 import Signup from "./pages/Signup.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Projects from "./pages/Projects.jsx";
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-
-
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/sign-in' element={<Signin/>}></Route>
-        <Route path='/sign-up' element={<Signup/>}></Route>
-        <Route path='/dashboard' element={<Dashboard/>}></Route>
-        <Route path='/projects' element={<Projects/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<Signup />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Route>
+        <Route path="/projects" element={<Projects />}></Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
