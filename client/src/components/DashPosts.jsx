e21b4fef1637675d7bc5b37e324b659e3830ffd4
Aniruddha Chatterjee <anichat1999@gmail.com>
2024-08-08 00:22:28 +0530

    Show more button functionality added

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showmore, setShowmore]=useState(true);
 
   //run this useEffect code each time the currentuser changes
   useEffect(() => {
@@ -18,6 +19,10 @@ function DashPosts() {
         console.log(data);
         if (res.ok) {
           setUserPosts(data.posts);
+          if(data.posts.length<9)
+          {
+            setShowmore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -30,6 +35,26 @@ function DashPosts() {
     }
   }, [currentUser._id]);
 
+  const handleShowMore=async()=>{
+    try {
+      const startIndex=userPosts.length;
+
+      const res=await fetch(`/api/post/getposts?userId=${currentUser.rest._id}&startIndex=${startIndex}`);
+
+      const data=await res.json();
+      
+      if(res.ok)
+      {
+        setUserPosts((prev)=>[...prev, ...data.posts]);
+        if(data.posts.length<9)
+        {
+          setShowmore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.rest.isAdmin && userPosts.length > 0 ? (
@@ -80,6 +105,10 @@ function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+          {
+            showmore && 
+            <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">Show More</button>
+          }
         </>
       ) : (
         <p>You have no posts yet!</p>
