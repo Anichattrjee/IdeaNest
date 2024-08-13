2459cbf40d2fbac6be29e0bded6e7da33c507628
Aniruddha Chatterjee <anichat1999@gmail.com>
2024-08-13 17:48:03 +0530

    Recent article section added

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,12 +3,14 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
 import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 
 function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -36,6 +38,22 @@ function PostPage() {
     fetchPost();
   }, [postSlug]);
 
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
   if (loading)
     return (
       <div className="flex justify-center min-h-screen items-center">
@@ -48,21 +66,43 @@ function PostPage() {
         {post && post.title}
       </h1>
 
-      <Link to={`/search?category=${post && post.category}`} className="self-center mt-5">
-        <Button color="gray" pill size="xs">{post && post.category}</Button>
+      <Link
+        to={`/search?category=${post && post.category}`}
+        className="self-center mt-5"
+      >
+        <Button color="gray" pill size="xs">
+          {post && post.category}
+        </Button>
       </Link>
 
-      <img src={post&& post.image} alt="Post-Image" className="w-full max-w-4xl mt-10 p-3 max-h-[600px] object-cover self-center" />
-      
+      <img
+        src={post && post.image}
+        alt="Post-Image"
+        className="w-full max-w-4xl mt-10 p-3 max-h-[600px] object-cover self-center"
+      />
+
       <div className="flex justify-between p-3 border-b border-slate-600 mx-auto w-full max-w-2xl text-xs">
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-        <span className="italic"> {post && (post.content.length/1000).toFixed(0)} mins read</span>
+        <span className="italic">
+          {" "}
+          {post && (post.content.length / 1000).toFixed(0)} mins read
+        </span>
       </div>
-      <div  dangerouslySetInnerHTML={{__html: post && post.content}} className="post-content w-full max-w-2xl p-3 mx-auto"></div>
+      <div
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+        className="post-content w-full max-w-2xl p-3 mx-auto"
+      ></div>
 
       <div className="">
-        <CallToAction/>
-        <CommentSection postId={post._id}/>
+        <CallToAction />
+      </div>
+      <CommentSection postId={post._id} />
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5">Recent Articles</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
       </div>
     </main>
   );
